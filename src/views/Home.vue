ff1f97119ffde2bab4d20a2f5f68af9803c18c02
fawzyzayed <fawzy@outlook.com>
2019-11-26 16:03:52 +0300

    add json

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,17 +2,34 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <select name="" id="">
+      <option value="" disabled selected>select country</option>
+      <option v-for="(country, index) in countriesName" :key="index">{{ country }}</option>
+    </select>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
+import countries from '../assets/jsons/countries.json'
+import axios from 'axios'
 
 export default {
   name: 'home',
+  data () {
+    return {
+      countriesName: []
+    }
+  },
   components: {
     HelloWorld
+  },
+  created () {
+    countries.forEach((element, index, array) => {
+      this.countriesName.push(element.name)
+      console.log(element.states)
+    })
   }
 }
 </script>
